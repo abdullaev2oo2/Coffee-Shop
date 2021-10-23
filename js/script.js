79dd8035d07bc9346a3b5f13a35e83ccd236e874
abdullaev2oo2 <76660654+abdullaev2oo2@users.noreply.github.com>
2021-10-24 04:20:57 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const mobileNav = document.querySelector("#burger-menu");
+const burgerBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const links = document.querySelectorAll(".mobile__nav-link");
+const searchBtn = document.querySelector("#search-icon");
+const inputBox = document.querySelector("#inputBox")
+
+links.forEach((link) => {
+    link.addEventListener("click", () => {
+        mobileNav.classList.remove("open")  
+    })
+})
+burgerBtn.addEventListener("click",() => {
+    mobileNav.classList.add("open")
+})
+closeBtn.addEventListener("click",() => {
+    mobileNav.classList.remove("open")
+})
+searchBtn.addEventListener("click", () => {
+    if (inputBox.style.display == "flex")
+        inputBox.style.display = "none"
+    else
+        inputBox.style.display = "flex"
+
+});
