d3301c950ceed3d4369d2c952ae938d0ee663565
abdullaev2oo2 <76660654+abdullaev2oo2@users.noreply.github.com>
2021-10-24 04:25:55 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,19 +3,19 @@ const burgerBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
 const links = document.querySelectorAll(".mobile__nav-link");
 const searchBtn = document.querySelector("#search-icon");
-const inputBox = document.querySelector("#inputBox")
+const inputBox = document.querySelector("#inputBox");
 
 links.forEach((link) => {
     link.addEventListener("click", () => {
         mobileNav.classList.remove("open")  
     })
-})
+});
 burgerBtn.addEventListener("click",() => {
     mobileNav.classList.add("open")
-})
+});
 closeBtn.addEventListener("click",() => {
     mobileNav.classList.remove("open")
-})
+});
 searchBtn.addEventListener("click", () => {
     if (inputBox.style.display == "flex")
         inputBox.style.display = "none"
