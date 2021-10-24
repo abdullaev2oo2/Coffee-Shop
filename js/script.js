387a99e173a09e6e3a3df55a806a9edf11840338
abdullaev2oo2 <76660654+abdullaev2oo2@users.noreply.github.com>
2021-10-24 14:12:30 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,22 +4,51 @@ const closeBtn = document.querySelector("#close-btn");
 const links = document.querySelectorAll(".mobile__nav-link");
 const searchBtn = document.querySelector("#search-icon");
 const inputBox = document.querySelector("#inputBox");
+const cartBtn = document.querySelector("#cart-icon");
+const menuCart = document.querySelector("#menu__cart");
 
 links.forEach((link) => {
     link.addEventListener("click", () => {
         mobileNav.classList.remove("open")  
     })
 });
+
+// Burger 
+
 burgerBtn.addEventListener("click",() => {
     mobileNav.classList.add("open")
 });
+burgerBtn.addEventListener("click",() => {
+    if (inputBox.style.display == "flex")
+        inputBox.style.display = "none";
+    if (menuCart.style.display == "block")
+        menuCart.style.display = "none";
+});
+
+// Mobile Navigation X
+
 closeBtn.addEventListener("click",() => {
     mobileNav.classList.remove("open")
 });
+
+// Search Button
+
 searchBtn.addEventListener("click", () => {
     if (inputBox.style.display == "flex")
         inputBox.style.display = "none"
     else
-        inputBox.style.display = "flex"
+        inputBox.style.display = "flex";
+    if (menuCart.style.display == "block")
+        menuCart.style.display = "none";
+});
 
+// Cart Btn
+
+cartBtn.addEventListener("click", () => {
+    if (menuCart.style.display == "block")
+        menuCart.style.display = "none"
+    else
+        menuCart.style.display = "block";
+    if (inputBox.style.display == "flex")
+        inputBox.style.display = "none";
 });
